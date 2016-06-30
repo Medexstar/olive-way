bcef34d4b24bd1a09247818889c32172cda89172
Derek Mui <derek678@hotmail.com>
2016-06-30 23:16:00 +1000

    Added newlines and indents
    Added newlines to files and indented code

--- app/models/suit.rb
+++ app/models/suit.rb
@@ -2,10 +2,9 @@ class Suit < ActiveRecord::Base
     has_and_belongs_to_many :orders
     
     # This method associates the attribute ":photo" with a file attachment
-  has_attached_file :photo, styles: {
-    big: '400x600>',
-    potrait: '200x300>',
-    landscape: '300x200>'
-  }
-
+    has_attached_file :photo, styles: {
+      big: '400x600>',
+      potrait: '200x300>',
+      landscape: '300x200>'
+    }
 end
