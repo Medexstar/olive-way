55ef886e0cd2e2c71f2a85a83561643446a183c9
Derek Mui <derek678@hotmail.com>
2016-07-04 01:01:23 +1000

    Added orders to orders page
    Fixed image style sizes for accessory model. Added orders to orders
    page.

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,7 +1,10 @@
 class OrdersController < ApplicationController
     def new
+        if !user_signed_in?
+          redirect_to account_path
+        end
         @order = Order.new
-        @order.user_id ||= current_user.id
+        @order.user ||= current_user
         @order_objects = OrderObject.pending(current_user);
     end
 end
