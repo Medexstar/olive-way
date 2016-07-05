e5adc6d3fde43511dc65d55e9f2042104d3fab46
hjsong6230 <hSong6230@gmail.com>
2016-07-05 15:03:33 +1000

    default values for address

--- app/controllers/address_controller.rb
+++ app/controllers/address_controller.rb
@@ -11,24 +11,24 @@ class AddressController < ApplicationController
     shipping = current_user.shipping_address
     shipping.first_name = params[:ship_firt_name] || ""
     shipping.last_name = params[:ship_last_name] || ""
-    shipping.postcode = params[:ship_postcode] || ""
+    shipping.postcode = params[:ship_postcode] || 0
     shipping.suburb = params[:ship_suburb] || ""
     shipping.company = params[:ship_company] || ""
     shipping.state = params[:ship_state] || ""
     shipping.street = params[:ship_street] || ""
     shipping.country = params[:ship_country] || ""
-    shipping.phone = params[:ship_phone] || ""
+    shipping.phone = params[:ship_phone] || 0
 
     billing = current_user.billing_address
     billing.first_name = params[:bill_firt_name] || ""
     billing.last_name = params[:bill_last_name] || ""
-    billing.postcode = params[:bill_postcode] || ""
+    billing.postcode = params[:bill_postcode] || 0
     billing.suburb = params[:bill_suburb] || ""
     billing.company = params[:bill_company] || ""
     billing.state = params[:bill_state] || ""
     billing.street = params[:bill_street] || ""
     billing.country = params[:bill_country] || ""
-    billing.phone = params[:bill_phone] || ""
+    billing.phone = params[:bill_phone] || 0
 
     if shipping.save! && billing.save!
       redirect_to profile_path
