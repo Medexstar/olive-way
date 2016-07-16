3540319ec869e9c51e6d090ede18a9aa736e0f6f
hjsong6230 <hSong6230@gmail.com>
2016-07-17 01:17:01 +1000

    re do measurement

---
+++ db/migrate/20160716150651_add_references_to_measurements.rb
@@ -0,0 +1,11 @@
+class AddReferencesToMeasurements < ActiveRecord::Migration
+  def self.up
+    add_reference :measurements, :user, index: true, foreign_key: true
+    add_reference :measurements, :order_object, index: true, foreign_key: true
+  end
+
+  def self.down
+    remove_reference :measurements, :user
+    remove_reference :measurements, :order_object
+  end
+end
