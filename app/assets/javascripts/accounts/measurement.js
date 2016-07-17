db39ab69432777d34d6182201a2f20de25910f69
Derek Mui <derek678@hotmail.com>
2016-07-17 18:49:35 +1000

    Made videos gender specific depending on the suit gender

--- app/assets/javascripts/accounts/measurement.js
+++ app/assets/javascripts/accounts/measurement.js
@@ -4,7 +4,7 @@ var measurement_videos = function(){
   }
 
   $("#measurement_neck").focus(function(){
-    if($("#user-gender").text()=="male"){
+    if(($("#user-gender").text()=="male") || ($("#suit-gender").text()=="male")){
       changeVideoURL("LOt9odA15pE");
     }else{
       changeVideoURL("cQ3t8dDKug8");
@@ -12,28 +12,28 @@ var measurement_videos = function(){
   });
 
   $("#measurement_shoulder").focus(function(){
-    if($("#user-gender").text()=="male"){
+    if(($("#user-gender").text()=="male") || ($("#suit-gender").text()=="male")){
       changeVideoURL("MJyS4NOBono");
     }else{
       changeVideoURL("D4rk9u0bwZE");
     }
   });
   $("#measurement_sleeve_length").focus(function(){
-    if($("#user-gender").text()=="male"){
+    if(($("#user-gender").text()=="male") || ($("#suit-gender").text()=="male")){
       changeVideoURL("HYL3EmHN0Uk");
     }else{
       changeVideoURL("Ofqdakg0Wac");
     }
   });
   $("#measurement_bicep").focus(function(){
-    if($("#user-gender").text()=="male"){
+    if(($("#user-gender").text()=="male") || ($("#suit-gender").text()=="male")){
       changeVideoURL("qrbTJJaou98");
     }else{
       changeVideoURL("xGeFkF8ySvY");
     }
   });
   $("#measurement_wrist").focus(function(){
-    if($("#user-gender").text()=="male"){
+    if(($("#user-gender").text()=="male") || ($("#suit-gender").text()=="male")){
       changeVideoURL("ZWAAlWhiU3o");
     }else{
       changeVideoURL("bn0OgTZ5u7w");
@@ -49,7 +49,7 @@ var measurement_videos = function(){
     changeVideoURL("Rzp5R-8e5vU");
   });
   $("#measurement_back_length").focus(function(){
-    if($("#user-gender").text()=="male"){
+    if(($("#user-gender").text()=="male") || ($("#suit-gender").text()=="male")){
       changeVideoURL("MgRxtD4SoTw");
     }else{
       changeVideoURL("mXS6JucO8_Y");
@@ -57,49 +57,49 @@ var measurement_videos = function(){
   });
 
   $("#measurement_waist").focus(function(){
-    if($("#user-gender").text()=="male"){
+    if(($("#user-gender").text()=="male") || ($("#suit-gender").text()=="male")){
       changeVideoURL("-3yWSdy-ESQ");
     }else{
       changeVideoURL("NvmOKHtaBkc");
     }
   });
   $("#measurement_hips").focus(function(){
-    if($("#user-gender").text()=="male"){
+    if(($("#user-gender").text()=="male") || ($("#suit-gender").text()=="male")){
       changeVideoURL("7f0N58nVULg");
     }else{
       changeVideoURL("grcEkz63SXU");
     }
   });
   $("#measurement_thigh").focus(function(){
-    if($("#user-gender").text()=="male"){
+    if(($("#user-gender").text()=="male") || ($("#suit-gender").text()=="male")){
       changeVideoURL("29O4lgy9t9Y");
     }else{
       changeVideoURL("f9mGeGE5Lrc");
     }
   });
   $("#measurement_rise").focus(function(){
-    if($("#user-gender").text()=="male"){
+    if(($("#user-gender").text()=="male") || ($("#suit-gender").text()=="male")){
       changeVideoURL("BHgnvqp-1BA");
     }else{
       changeVideoURL("-bMmuHAXnos");
     }
   });
   $("#measurement_outleg").focus(function(){
-    if($("#user-gender").text()=="male"){
+    if(($("#user-gender").text()=="male") || ($("#suit-gender").text()=="male")){
       changeVideoURL("TgMaiwl81Vs");
     }else{
       changeVideoURL("6hMRaLCCsw0");
     }
   });
   $("#measurement_inleg").focus(function(){
-    if($("#user-gender").text()=="male"){
+    if(($("#user-gender").text()=="male") || ($("#suit-gender").text()=="male")){
       changeVideoURL("bfEn7gk_BeI");
     }else{
       changeVideoURL("x4QCHafwrwQ");
     }
   });
   $("#measurement_ankle").focus(function(){
-    if($("#user-gender").text()=="male"){
+    if(($("#user-gender").text()=="male") || ($("#suit-gender").text()=="male")){
       changeVideoURL("T2rkEWGfs-w");
     }else{
       changeVideoURL("h_r4MqS-l_A");
