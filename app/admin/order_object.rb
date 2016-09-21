a612b2ce7ec903ba81d2793051b3942e1ac474b2
Charlton Groves <charltongroves@gmail.com>
2016-09-21 14:56:02 +1000

    test fix

--- app/admin/order_object.rb
+++ app/admin/order_object.rb
@@ -15,52 +15,52 @@ ActiveAdmin::register OrderObject do
     column :jacket_buttons
     column :status
     column 'Neck' do |oo|
-      oo.measurement.neck unless oo.measurement.nil?
+      oo.measurement.neck*100 unless oo.measurement.nil?
     end
     column 'Shoulder' do |oo|
-      oo.measurement.shoulder unless oo.measurement.nil?
+      oo.measurement.shoulder*100 unless oo.measurement.nil?
     end
     column 'Chest' do |oo|
-      oo.measurement.chest unless oo.measurement.nil?
+      oo.measurement.chest*100 unless oo.measurement.nil?
     end
     column 'Sleeve Length' do |oo|
-      oo.measurement.sleeve_length unless oo.measurement.nil?
+      oo.measurement.sleeve_length*100 unless oo.measurement.nil?
     end
     column 'Bicep' do |oo|
-      oo.measurement.bicep unless oo.measurement.nil?
+      oo.measurement.bicep*100 unless oo.measurement.nil?
     end
     column 'Wrist' do |oo|
-      oo.measurement.wrist unless oo.measurement.nil?
+      oo.measurement.wrist*100 unless oo.measurement.nil?
     end
     column 'Bust' do |oo|
-      oo.measurement.bust unless oo.measurement.nil?
+      oo.measurement.bust*100 unless oo.measurement.nil?
     end
     column 'Underbust' do |oo|
-      oo.measurement.underbust unless oo.measurement.nil?
+      oo.measurement.underbust*100 unless oo.measurement.nil?
     end
     column 'Back Length' do |oo|
-      oo.measurement.back_length unless oo.measurement.nil?
+      oo.measurement.back_length*100 unless oo.measurement.nil?
     end
     column 'Waist' do |oo|
-      oo.measurement.waist unless oo.measurement.nil?
+      oo.measurement.waist*100 unless oo.measurement.nil?
     end
     column 'Hips' do |oo|
-      oo.measurement.hips unless oo.measurement.nil?
+      oo.measurement.hips*100 unless oo.measurement.nil?
     end
     column 'Thigh' do |oo|
-      oo.measurement.thigh unless oo.measurement.nil?
+      oo.measurement.thigh*100 unless oo.measurement.nil?
     end
     column 'Rise' do |oo|
-      oo.measurement.rise unless oo.measurement.nil?
+      oo.measurement.rise*100 unless oo.measurement.nil?
     end
     column 'Out Leg' do |oo|
-      oo.measurement.outleg unless oo.measurement.nil?
+      oo.measurement.outleg*100 unless oo.measurement.nil?
     end
     column 'In Leg' do |oo|
-      oo.measurement.inleg unless oo.measurement.nil?
+      oo.measurement.inleg*100 unless oo.measurement.nil?
     end
     column 'Ankle' do |oo|
-      oo.measurement.ankle unless oo.measurement.nil?
+      oo.measurement.ankle*100 unless oo.measurement.nil?
     end
   end
 end
